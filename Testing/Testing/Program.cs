5cdbf4d48de78fbe172fd1d22706de2616756856
RooTender <lewandowski.hubert486@gmail.com>
2018-11-15 13:57:49 +0100

    initial upload

---
+++ Testing/Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyVector;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing MyVector.dll");
+            Vector Vector = new Vector(20, 20);
+            Console.WriteLine("Length equals: " + Vector.length());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vector/Vector/MyVector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyVector
+{
+    public class Vector
+    {
+        public Vector(int x, int y)
+        {
+            this.x = (double) x;
+            this.y = (double) y;
+        }
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector() { }
+
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public double length()
+        {
+            return Math.Sqrt((x * x) + (y * y));
+        }
+    }
+}
